d05dd7df017eedabd2dd39302935c502fd8543a3
sergiokno <checho042190@gmail.com>
2024-10-12 14:33:18 -0500

    se creo el menu propietarios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Propietarios from './Propietarios.jsx'
+import './Propietarios.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/> 
+    <Propietarios></Propietarios>
   </StrictMode>,
 )
