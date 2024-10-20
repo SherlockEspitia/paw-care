2e0270a9b593cf54f60f1c99c0119716f25f0e31
Sherlockespitia <claw.1991@gmail.com>
2024-10-19 23:00:28 -0500

    Proceso migracion de los componentes visibles del prototipo y un layaout

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+function Header(){
+
+    return (
+        <header>
+            <img src="/src/img/Logo.svg" alt="AppCare" className='logo' height={100} width={100}/>
+            <nav>
+                <a href="/">Inicio</a>
+                <a href="">Servicios</a>
+                <a href="">Tienda</a>
+                <a href="">Educativo</a>
+                <a href="">Sobre Nosotros</a>
+            </nav>
+        </header>
+    )
+
+        
+}
+
+export default Header
