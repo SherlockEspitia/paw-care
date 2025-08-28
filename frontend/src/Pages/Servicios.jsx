87062fd66ae9e19fb22dea8050d7a0ec8f22ce7d
sergiokno <checho042190@gmail.com>
2025-08-28 16:11:22 -0500

    se modifica el html de servicios para compatibilidad con react, se instala la dependencia react-router-dom para las rutas, navegacion y se crea la carpeta Pages

---
+++ frontend/src/Pages/Servicios.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Servicios as ServiciosComponent } from '../components/Servicios/Servicios';
+
+function Servicios() {
+    return <ServiciosComponent />;
+}
+
+export default Servicios;
