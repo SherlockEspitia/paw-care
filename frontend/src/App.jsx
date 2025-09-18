e72fc6e8fa8176908a6ddfabc99dfc85fd6fb511
sergiokno <checho042190@gmail.com>
2025-09-18 09:32:08 -0500

    Se actualiza app.jsx dando la ruta de Inicio de sesion de usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import './App.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InicioSesion from './components/InicioSesion/InicioSesion';
 
 import Home from './Pages/Home';
 import Servicios from './Pages/Servicios';
 import Galeria from './Pages/Galeria';
+import Contacto from './Pages/Contacto';
+
 
 function App() {
   return (
@@ -16,9 +19,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/servicios" element={<Servicios />} />
         <Route path="/galeria" element={<Galeria />} />
+        <Route path="/contacto" element={<Contacto/>} />
+        <Route path="/login" element={<InicioSesion />} />
       </Routes>
       <Footer />
-    </Router>
+    </Router>    
   );
 }
 
