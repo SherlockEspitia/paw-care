d8822343c27a9073bd5185e0933f0b6a1c720a12
sergiokno <checho042190@gmail.com>
2025-09-19 14:51:07 -0500

    Se incorpora las rutas de Registro y su importacion, al igual que las rutas de formularios de cuidadores y propietarios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import InicioSesion from './components/InicioSesion/InicioSesion';
+import Registro from './components/Registro/Registro';
+import Propietarios from './components/FormPropietarios/Propietarios';
+import Cuidadores from './components/FormCuidadores/Cuidadores';
 
 import Home from './Pages/Home';
 import Servicios from './Pages/Servicios';
@@ -21,6 +24,9 @@ function App() {
         <Route path="/galeria" element={<Galeria />} />
         <Route path="/contacto" element={<Contacto/>} />
         <Route path="/login" element={<InicioSesion />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/registro/propietario" element={<Propietarios />} /> 
+        <Route path="/registro/cuidador" element={<Cuidadores />} />
       </Routes>
       <Footer />
     </Router>    
